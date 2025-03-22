7ff1bfc3ed1f2de7328ff1866b5c717c1e5969a0
mridulxtiwari <mridulxtiwari@gmail.com>
2025-03-22 18:48:46 +0530

    Basic project structure

---
+++ src/main/java/com/springboot/projects/postgres_setup/PostgresSetupApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.projects.postgres_setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresSetupApplication.class, args);
+	}
+
+}
